57ec6de3510b6ac69b30ef2980c306dccb506082
Waiting7777 <waiting0819@msn.com>
2017-07-05 12:21:09 +0800

    gif test

--- src/index.js
+++ src/index.js
@@ -14,11 +14,10 @@ $(document).ready(function(){
     let canvas = document.getElementById('back')
     let ctx = canvas.getContext('2d')
     let img = new Image()
-    let under = new Image()
+    let under1 = new Image()
+    let under2 = new Image()
+    let under3 = new Image()
     let scroll_now
-    let under1 = document.getElementById('under-1')
-    let under2 = document.getElementById('under-2')
-    let under3 = document.getElementById('under-3')
     let playing = false
     let playInt = null
 
@@ -27,11 +26,15 @@ $(document).ready(function(){
         $('#back').prop('width' ,720)
 	    $('#back').prop('height', 1280)
 
+        $('.back').css('margin-top',  -(600 * w / 720)/2 + 'px')
+
         img.src = 'assets/bg.jpg'
         img.addEventListener("load", function() {
   			ctx.drawImage(img, 0, 0, 720, 1280, 0, 0, 720, 1280);
 		}, false);
-        under.src = 'assets/under1.jpg'
+        under1.src = 'assets/under1.gif'
+        under2.src = 'assets/under2.gif'
+        under3.src = 'assets/under3.gif'
 
         $(window).on('scroll', function(){
 
@@ -45,54 +48,47 @@ $(document).ready(function(){
                 ctx.drawImage(img, 0, 0, 720, 1280, 0, 0, 720, 1280);
                 $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0)')
             }
+            else if(intro >= 150 && intro < h){
+                $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0.8)')
+            }
             else{
                 $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0.8)')
             }
 
             if(first < 150){
-                ctx.clearRect(0, 0, 720, 1280)
-                ctx.drawImage(img, 0, 0, 720, 1280, 0, 0, 720, 1280);
+                $('.back').css('opacity', 0)
+                $('#back').css('opacity', 1)
+                $('#b-title').css('opacity', 0)
             }
-            else if(first >= 150 && first < h*1.5 - 150){
+            else if(first >= 150 && first < h*1.2){
                 $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0.8)')
-                ctx.clearRect(0, 0, 720, 1280)
-                ctx.drawImage(under1, 0, 0, 720, 1280, 0, 0, 720, 1280);
-                clearInterval(playInt)
-                playing = false
-                under1.pause()
+                $('#back').css('opacity', 1)
+                $('.back').css('opacity', 0)
+                $('#b-title').css('opacity', 0)
             }
-            else if(first >= h*1.5 - 150 && first < h*2 + 300){
+            else if(first >= h*1.2 && first < h *1.8){
                 $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0)')
-                if(playing == false){
-                    playing = true
-                    under1.play()
-                    playInt = setInterval(function(){
-                        ctx.clearRect(0, 0, 720, 1280)
-                        ctx.drawImage(under1, 0, 0, 720, 1280, 0, 0, 720, 1280);
-                        console.log(5)
-                    }, 100)
-                }
+                $('.back').css('opacity', 0)
+                $('#under-1').css('opacity', 1)
+                $('#back').css('opacity', 0)
+                $('#b-title').css('opacity', 1)
+                $('#b-title').text('長距離')
             }
-            else if(first >= h*2 + 300 && first < h*2.5 + 300){
-                $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0.8)')
-                ctx.clearRect(0, 0, 720, 1280)
-                ctx.drawImage(under2, 0, 0, 720, 1280, 0, 0, 720, 1280);
-                clearInterval(playInt)
-                playing = false
-                under1.pause() 
-                under2.pause()
+            else if(first >= h*1.8 && first < h * 3){
+                $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0)')
+                $('.back').css('opacity', 0)
+                $('#under-2').css('opacity', 1)
+                $('#back').css('opacity', 0)
+                $('#b-title').css('opacity', 1)
+                $('#b-title').text('短距離')
             }
             else{
                 $('.bg-b').css('background-color', 'rgba(0, 0, 0, 0)')
-                if(playing == false){
-                    playing = true
-                    under2.play()
-                    playInt = setInterval(function(){
-                        ctx.clearRect(0, 0, 720, 1280)
-                        ctx.drawImage(under2, 0, 0, 720, 1280, 0, 0, 720, 1280);
-                        console.log(5)
-                    }, 100)
-                }
+                $('.back').css('opacity', 0)
+                $('#under-3').css('opacity', 1)
+                $('#back').css('opacity', 0)
+                $('#b-title').css('opacity', 1)
+                $('#b-title').text('上端')
             }
 
             console.log(first)

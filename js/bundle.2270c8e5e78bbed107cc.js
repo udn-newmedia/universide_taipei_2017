7f3a32288f4efba145d38fdad3481ecb6d3e0281
Waiting7777 <waiting0819@msn.com>
2017-07-12 18:56:21 +0800

    Change to page"

---
+++ js/bundle.2270c8e5e78bbed107cc.js
@@ -0,0 +1,36 @@
+webpackJsonp([0],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(2);
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+__webpack_require__(4);
+
+__webpack_require__(5);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _jquery2.default)(document).ready(function () {
+
+    var w = (0, _jquery2.default)(window).width();
+    var h = (0, _jquery2.default)(window).height();
+
+    (0, _jquery2.default)('#fullpage').fullpage();
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[1]);

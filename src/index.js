243b2b92c6a29d99089cd83495d2721769d1caf7
Waiting7777 <waiting0819@msn.com>
2017-07-21 18:46:33 +0800

    mobile 90%

--- src/index.js
+++ src/index.js
@@ -21,10 +21,12 @@ $(document).ready(function(){
 
     const w = $(window).width()
     const h = $(window).height()
+
+    $('.f-video-contain').css('height', 600*w/h + 'px')
     
     $('#fullpage').fullpage({
         navigation: false,    	
-        scrollOverflow : true,
+        scrollOverflow : false,
     	afterLoad: function(anchorLink, index){
             if(index == 1){
                 $('#cover-title').css('opacity', 1)
@@ -50,7 +52,6 @@ $(document).ready(function(){
                 $('#section-3 .box-container').css('transform', 'translate(0, 0)')
                 $('#section-4 .popup').css('transform', 'translate(0, 100px)')
             }
-            
     		if(index == 4){
                 $('#section-3 .box-container').css('transform', 'translate(0, 50px)')
                 $('#movie-4').get(0).play()
@@ -59,12 +60,59 @@ $(document).ready(function(){
     		if(index == 5){
                 $('#movie-5').get(0).play()
                 $('#section-4 .popup').css('transform', 'translate(0, 100px)')
-                $('#section-6 .popup').css('transform', 'translate(0, 100px)')
     		}
     		if(index == 6){
-                $('#movie-6').get(0).play()
                 $('#section-5 .popup').css('transform', 'translate(0, 100px)')
-    		}
+                $('#section-7 .box-container').css('transform', 'translate(0, 50px)')
+            }
+            if(index == 7){
+                $('#section-7 .box-container').css('opacity', 1)
+                $('#section-7 .box-container').css('transform', 'translate(0, 0)')
+                $('#section-8 .popup').css('transform', 'translate(0, 100px)')
+            }
+            if(index == 8){
+                $('#section-7 .box-container').css('transform', 'translate(0, 50px)')
+                $('#movie-8').get(0).play()
+                $('#section-9 .box-container').css('transform', 'translate(0, 50px)')
+            }
+            if(index == 9){
+                $('#section-9 .box-container').css('opacity', 1)
+                $('#section-9 .box-container').css('transform', 'translate(0, 0)')
+                $('#section-8 .popup').css('transform', 'translate(0, 100px)')
+                $('#section-10 .popup').css('transform', 'translate(0, 100px)')
+            }
+            if(index == 10){
+                $('#section-9 .box-container').css('transform', 'translate(0, 50px)')
+                $('#movie-10').get(0).play()
+                $('#section-11 .popup').css('transform', 'translate(0, 100px)')
+            }
+            if(index == 11){
+                $('#movie-11').get(0).play()
+                $('#section-10 .popup').css('transform', 'translate(0, 100px)')
+            }
+            if(index == 12){
+                $('#section-12 .box-container').css('opacity', 1)
+                $('#section-12 .box-container').css('transform', 'translate(0, 0)')
+                $('#section-11 .popup').css('transform', 'translate(0, 100px)')
+            }
+            if(index == 13){
+                $('#section-12 .box-container').css('transform', 'translate(0, 50px)')
+                $('#movie-13').get(0).play()
+                $('#section-13 .box-container').css('transform', 'translate(0, 50px)')
+            }
+            if(index == 14){
+                $('#section-14 .box-container').css('opacity', 1)
+                $('#section-14 .box-container').css('transform', 'translate(0, 0)')
+                $('#section-15 .popup').css('transform', 'translate(0, 100px)')
+            }
+            if(index == 15){
+                $('#section-14 .box-container').css('transform', 'translate(0, 50px)')
+                $('#movie-15').get(0).play()
+            }
+            if(index == 16){
+                $('#section-15 .popup').css('transform', 'translate(0, 100px)')
+            }
+
         },
         onLeave: function(index, nextIndex, direction){
             console.log(index, nextIndex, direction)
@@ -94,11 +142,67 @@ $(document).ready(function(){
                 $('#section-5 .popup').css('transform', 'translate(0, 0)')
             }
             if(index == 5){
-                $('#section-6 .popup').css('transform', 'translate(0, 0)')
                 $('#section-4 .popup').css('transform', 'translate(0, 0)')
             }
             if(index == 6){
                 $('#section-5 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 6 && direction == 'down'){
+                $('#section-7 .yellow-back').css('height', '100vh')
+            }
+            if(index == 7 && direction == 'up'){
+                $('#section-7 .yellow-back').css('height', 0)
+            }
+            if(index == 7){
+                $('#section-7 .box-container').css('opacity', 0)
+                $('#section-8 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 8 && direction == 'down'){
+                $('#section-9 .yellow-back').css('height', '100vh')
+            }
+            if(index == 8){
+                $('#section-8 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 9 && direction == 'up'){
+                $('#section-9 .yellow-back').css('height', 0)
+            }
+            if(index == 9){
+                $('#section-9 .box-container').css('opacity', 0)
+                $('#section-8 .popup').css('transform', 'translate(0, 0)')
+                $('#section-10 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 10){
+                $('#section-11 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 11 && direction == 'down'){
+                $('#section-12 .yellow-back').css('height', '100vh')
+            }
+            if(index == 11){
+                $('#section-10 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 12 && direction == 'up'){
+                $('#section-12 .yellow-back').css('height', 0)
+            }
+            if(index == 12){
+                $('#section-12 .box-container').css('opacity', 0)
+                $('#section-11 .popup').css('transform', 'translate(0, 0)')
+                $('#section-13 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 13){
+                $('#section-14 .yellow-back').css('height', '100vh')
+            }
+            if(index == 14 && direction == 'up'){
+                $('#section-14 .yellow-back').css('height', 0)
+            }
+            if(index == 14){
+                $('#section-14 .box-container').css('opacity', 0)
+                $('#section-15 .popup').css('transform', 'translate(0, 0)')
+            }
+            if(index == 15){
+
+            }
+            if(index == 16){
+                $('#section-15 .popup').css('transform', 'translate(0, 0)')
             }
            
             
